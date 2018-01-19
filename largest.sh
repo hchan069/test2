f189a39cccbdc38ae06c7dc58135c4866403eb7b
Ho Yee Chan <hchan069@hammer.cs.ucr.edu>
2018-01-18 18:39:03 -0800

    added finding largest number

---
+++ largest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
+   echo "Please pass exactly 3 parameters"
+elif [ "$1" -ge "$2" ] && [ "$1" -gt "$3" ]; then
+   echo $1
+elif [ "$2" -ge "$1" ] && [ "$2" -gt "$3" ]; then
+   echo $2
+elif [ "$3" -ge "$1" ] && [ "$3" -gt "$2" ]; then
+   echo $3
+else
+   echo "They are all equal"
+fi
