d79c5ce208a89321f02f84ac065a804d0abcc189
Ho Yee Chan <hchan069@hammer.cs.ucr.edu>
2018-01-18 19:08:30 -0800

    fixed compare negative numbers

--- largest.sh
+++ largest.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+
+re='^[0-9]+([.][0-9]+)?$'
+for param in "$@"
+do
+   if ! [[ $param =~ $re ]]; then
+      echo "error: please pass in a number.";
+      exit 1
+   fi
+done
+
 if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
    echo "Please pass exactly 3 parameters"
-elif [ "$1" -ge "$2" ] && [ "$1" -gt "$3" ]; then
+elif [[ $(echo "$1 >= $2" | bc) -eq 1 && $(echo "$1 > $3" | bc) -eq 1 ]]; then
    echo $1
-elif [ "$2" -ge "$1" ] && [ "$2" -gt "$3" ]; then
+elif [[ $(echo "$2 >= $1" | bc) -eq 1 && $(echo "$2 > $3" | bc) -eq 1 ]]; then
    echo $2
-elif [ "$3" -ge "$1" ] && [ "$3" -gt "$2" ]; then
+elif [[ $(echo "$3 >= $2" | bc) -eq 1 && $(echo "$3 > $1" | bc) -eq 1 ]]; then
    echo $3
 else
    echo "They are all equal"
 fi
+exit
